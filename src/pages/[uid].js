1be7f022aa74d119a9cb28d27e7c402bb185740d
UmerGillani36 <syedomershahgillani@gmail.com>
2023-06-12 18:48:50 +0500

    server side props

---
+++ src/pages/[uid].js
@@ -0,0 +1,19 @@
+const UserIdPage = (props) => {
+  return (
+    <>
+      <h1>{props.id}</h1>
+    </>
+  );
+};
+
+export default UserIdPage;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const userId = params.uid;
+  return {
+    props: {
+      id: 'userid-' + userId,
+    },
+  };
+}
